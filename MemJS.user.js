58e85fc0cc959a1e1c2b889e3aab2f1ba82e2912
Tayyib <m.tayyib.yel@gmail.com>
2017-08-28 22:15:01 +0300

    Updated the userscript.

--- MemJS.user.js
+++ MemJS.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name          MemJS
-// @version       0.2
+// @version       0.3
 // @description   Download Memrise courses. For users and creators.
 // @author        Tayyib <m.tayyib.yel@gmail.com>
 // @copyright     Licensed under Apache 2.0
-// @namespace     https://tampermonkey.net
+// @namespace     https://github.com/Tayyib
 // @homepageURL   https://github.com/Tayyib/MemJS
 // @updateURL     https://github.com/Tayyib/MemJS/raw/master/MemJS.user.js
 // @downloadURL   https://github.com/Tayyib/MemJS/raw/master/MemJS.user.js
@@ -19,10 +19,9 @@ Forked from github.com/scytalezero/MemriseUtilities
 
 var editPageRegex = /.*\/edit\/.*/i;
 var databasePageRegex = /.*\/edit\/database\/.*/i;
-var onProgress = false;
-var ButtonGet;
-var levelCount;
 var dataURL;
+var levelCount;
+var step;
 
 (function ()
 {
@@ -30,17 +29,17 @@ var dataURL;
 
     if (document.URL.search(databasePageRegex) === 0)
     {
-        dataURL = document.URL + "?page=%s";
+        dataURL = document.URL + '?page=%s';
         levelCount = $(".pagination-centered li:nth-last-child(2)").text().replace(/\s+/g, '');
     }
     else if (document.URL.search(editPageRegex) === 0)
     {
-        dataURL = document.URL + "%s";
+        dataURL = document.URL + '%s';
         levelCount = $(".level.collapsed:last-child .level-handle")[0].textContent;
     }
     else if ($(".levels.clearfix").length !== 0)
     {
-        dataURL = document.URL + "%s";
+        dataURL = document.URL + '%s';
         levelCount = $(".level:last-child .level-index")[0].textContent;
     }
     else
@@ -49,36 +48,63 @@ var dataURL;
         return;
     }
 
-    $("ul.nav-pills").append("<li><a class='tab' id='ButtonGet' href='#' style='font-weight: bold;'>Get Bulk</a></li>");
+    PrepareUI();
 
-    ButtonGet = $("#ButtonGet");
-    ButtonGet.click(OnClick);
+    function PrepareUI()
+    {
+        var htmlButton = "<li><button class='tab' id='MemJS-Button' style='font-weight:bold;'>Download!</button></li>";
+        var htmlMemJS =
+            "<div id='MemJS-UI'>" +
+            "<div id='MemJS-Header'>" +
+            "<h3 id='MemJS-Title' style='display:inline;'>Loading...</h3>" +
+            "<button style='float:right;'>Copy Data</button>" +
+            "</div>" +
+            "<textarea id='MemJS-TextArea' style='width:100%; height:222px;'></textarea>" +
+            "</div>";
+
+        $('ul.nav-pills').append(htmlButton);
+        $('div.container-main').prepend(htmlMemJS);
+        // $('#MemJS-UI').hide();
+
+        $('#MemJS-Button').click(OnClick);
+    }
 
     function OnClick()  // TODO
     {
-        if (onProgress)
+        $('#MemJS-Button').unbind('click');
+        $('#MemJS-Button').text("In progress...");
+
+        step = 0;
+
+        for (var i = 1; i <= levelCount; i++)
         {
-            print("Already in progress");
-            return;
+            // @formatter:off
+            $.ajax({
+                dataType: 'html',
+                url: dataURL.replace(/%s/, i),
+                success: AbstractData,
+                beforeSend: function (jqXHR, settings) { jqXHR.level = i; }
+            });
+            // @formatter:on
         }
-
-        onProgress = true;
-
-        ButtonGet.text("In progress...");
-        ButtonGet.unbind("click");
-
-        // TODO - ajax, push, promises, async=?
     }
 
-    function AbstractData()
+    function AbstractData(data, status, jqXHR) // TODO
     {
-        // TODO
+        step++;
+        $('#MemJS-Title').html("Loading... " + step + "/" + levelCount);
+
+        $(data).find("tbody.things > tr div.text").each(function (index)  // fixme!
+        {
+            // TODO
+        });
+
+        if (step === levelCount) OnFinish();
     }
 
     function OnFinish()  // TODO
     {
-        onProgress = false;
-        ButtonGet.bind("click");
+        $('#MemJS-Button').bind('click');
     }
 })();
 
